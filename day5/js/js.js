37c44bb0b97776ededa212a49d39e5a341c8fbd8
arypte <wjs505@gmail.com>
2023-03-21 20:54:48 +0900

    day5_js
    day5_js

---
+++ day5/js/js.js
@@ -0,0 +1,19 @@
+const chains = [
+  "mainnet",
+  "polygon",
+  "avalanche",
+  "arbitrum",
+  "gnosis",
+  "bsc",
+  "optimism",
+  "fantom",
+];
+
+const myProfile = {
+  name: "JDH",
+  job: "programmer",
+  age: 18,
+  isKind: true,
+};
+
+console.log(myProfile);
---
+++ day5/js/variables.js
@@ -0,0 +1,7 @@
+let myname = "JDH";
+
+if (true) {
+  let myname = "h662";
+}
+
+console.log(myname);
